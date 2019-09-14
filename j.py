bc723df028304bfeb72eb4fc4ab97187195d5e62
Harish <harish_dhobe@neovasolutions.com>
2019-09-14 17:06:06 +0530

    comment by 1st user

--- j.py
+++ j.py
@@ -1,3 +1,4 @@
 a = '{"_t": 1557342421, "url": "https://www.sdxcentral.com/resources/sponsored/webinars/infoblox-expand-hybrid-cloud/", "tags": "2019,cloud,infoblox,sdn-software-defined-networking,security,sponsored,webinar", "_n": "view_article", "l": "OTYuODcuMzQuMjQ5", "title": "Webinar: Modernize Network Services and Expand Hybrid Cloud Options", "campaign_id": "6638", "_p": "91c9d34b-0752-4b14-ba11-2b5b8239c503"}'
 if '_p' in a:
     print("zxcz")
+# comment added by 1st user 
